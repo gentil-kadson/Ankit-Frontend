5a5555acaa4336215fb24a8cdc29198fda8a6078
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-06-11 14:52:12 -0300

    feature: login page with title already inserted.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,15 @@
+import styled from "styled-components";
+
 export default function Login() {
-  return <div>This is the login page.</div>;
+  return (
+    <MainWrapper>
+      <div className="title-container">
+        <h1>Ankit</h1>
+      </div>
+    </MainWrapper>
+  );
 }
+
+const MainWrapper = styled.main`
+  border: 2px solid white;
+`;
