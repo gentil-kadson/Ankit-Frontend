cbd49eb8bc9c0104cb447e6f291aeaf1eb5d0ffc
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-06-21 12:02:03 -0300

    button component finished.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,45 @@ import React from "react";
 import styled from "styled-components";
 
 type Props = {
-  width: number;
+  width: string;
   children: React.ReactNode;
+  inverted?: boolean;
+  onClick?: () => {};
 };
 
-export default function Button({ children, width }: Props) {
-  return <CustomButton width={width}>{children}</CustomButton>;
+export default function Button({
+  children,
+  width,
+  onClick,
+  inverted = false,
+}: Props) {
+  return (
+    <CustomButton
+      width={width}
+      inverted={inverted ? inverted : undefined}
+      onClick={onClick && onClick}
+    >
+      {children}
+    </CustomButton>
+  );
 }
 
-const CustomButton = styled.button<{ width: number }>`
+const CustomButton = styled.button<{ width: string; inverted?: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;
+  border-radius: 0.625rem;
+  padding: 12px 0px;
 
-  background-color: red;
-  border: none;
-  width: ${(props) => props.width + "rem"};
+  font-size: 1.25rem;
+  font-weight: bold;
+
+  background-color: ${(props) => {
+    return props.inverted ? "var(--background)" : "var(--blue)";
+  }};
+  border: ${(props) => {
+    return props.inverted ? "3px solid var(--blue)" : "none";
+  }};
+  width: ${(props) => props.width};
+  color: ${(props) => (props.inverted ? "var(--blue)" : "var(--white)")};
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Button from "@/components/Button";
 
 export default function Home() {
-  return <Button width={10}>Valha mds</Button>;
+  return <Button width="13rem">Valha mds</Button>;
 }
