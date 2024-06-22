ea2cf5f223e7e4e3e371eeb84d6d85e5e26d7b2d
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-06-22 11:47:35 -0300

    feature: adds initial styles to select component.

---
+++ src/components/Select.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import { MaterialSymbol } from "react-material-symbols";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Select({ children, ...props }: Props) {
+  return <SelectInput {...props}>{children}</SelectInput>;
+}
+
+const SelectInput = styled.select`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 0.375rem;
+
+  appearance: none;
+  background-image: url("selectArrow.svg");
+  background-repeat: no-repeat;
+  background-position: right 0.7rem top 50%;
+  background-size: 1.5rem auto;
+`;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,12 @@
+import Select from "@/components/Select";
+
 export default function Login() {
-  return <p>Welcome to Login Page.</p>;
+  return (
+    <Select>
+      <option value="">Escolha uma opção</option>
+      <option value="camarão">Camarão</option>
+      <option value="enguia">Enguia</option>
+      <option value="peixe-espada">Peixe espada</option>
+    </Select>
+  );
 }
