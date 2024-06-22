c8d388ab57be05375bdb2f5a3e73f925ab9828f2
Gentil Ályson <fernandes.gentilalysonjac@gmail.com>
2024-06-22 13:06:42 -0300

    feature: adds FlagItem, LanguageStatisticRow and StatisticsCard components

---
+++ src/components/StatisticsCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+  title: string;
+  children: React.ReactNode;
+};
+
+export default function StatisticsCard({ title, children }: Props) {
+  return (
+    <StatisticsSection>
+      <h2>{title}</h2>
+      <StatisticsCardsGroup>{children}</StatisticsCardsGroup>
+    </StatisticsSection>
+  );
+}
+
+const StatisticsSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    color: var(--blue);
+    font-weight: bold;
+  }
+`;
+
+const StatisticsCardsGroup = styled.div``;
