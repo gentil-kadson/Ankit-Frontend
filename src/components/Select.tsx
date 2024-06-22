3fb65b21de378c677b41eccda6706927d50b0291
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-06-22 11:59:29 -0300

    feature: adds select input.

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { MaterialSymbol } from "react-material-symbols";
 
 type Props = {
   children: React.ReactNode;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,3 @@
-import Select from "@/components/Select";
-
 export default function Login() {
-  return (
-    <Select>
-      <option value="">Escolha uma opção</option>
-      <option value="camarão">Camarão</option>
-      <option value="enguia">Enguia</option>
-      <option value="peixe-espada">Peixe espada</option>
-    </Select>
-  );
+  return <div>This is the login page.</div>;
 }
