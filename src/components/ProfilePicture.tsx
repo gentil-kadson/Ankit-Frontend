0125cf5d1e442304678cce76583b1e2638ccd9e1
Gentil Ályson <fernandes.gentilalysonjac@gmail.com>
2024-06-23 12:04:39 -0300

    fix: makes width and height user set in ProfilePicture component

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -3,15 +3,17 @@ import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 type Props = {
   src: string | StaticImport;
+  width: number;
+  height: number;
 };
 
-export default function ProfilePicture({ src }: Props) {
+export default function ProfilePicture({ src, width, height }: Props) {
   return (
     <Image
       src={src}
       alt="User profile picture"
-      width={80}
-      height={80}
+      width={width}
+      height={height}
       style={{ border: "solid 2px var(--blue)", borderRadius: 50 }}
     />
   );
